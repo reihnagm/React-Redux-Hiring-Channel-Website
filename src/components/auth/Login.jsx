80184c4852b8b296c4fbaeff1fa9079ced1a01d0
unknown <reihanagam>
2020-01-05 10:38:46 +0700

    new design for login page

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -16,25 +16,58 @@ const Login = ({ login, isAuthenticated }) => {
     const onChange = event => setFormData({ ...formData, [event.target.name]: event.target.value })
 
     const onSubmit = event => {
+        console.log(email, password)
         event.preventDefault()
         login(email, password);
     }
 
     if (isAuthenticated) {
-        return <Redirect to='/' />;
+        return <Redirect to='/engineers' />;
     }
 
     return (
         <Fragment>
-            <div className='container'>
 
-                <header className='navbar has-small-vm'>
-                    <img src='./logo.png' alt="" id='logo' className='img-brand' />
-                </header>
+            <div className='columns'>
+                <div id='cover-background-login' className='column is-marginless'>
+                    <div id='cover-login'></div>
+                    <h2 id='title-cover-login'>Hire expert freelancers for any job, online</h2>
+                    <p id='sub-title-cover-login'>Millions of small businesses use Frelancer to turn their ideas into reality.</p>
+                </div>
+                <div id='content-login' className='column is-marginless'>
+                    <h2 id='title-content-login'>Login</h2>
 
-                <Alert />
+                <form onSubmit={e => onSubmit(e)}>
+                    <div id='login-form' className='columns is-direction-column'>
+                        <div className='column is-marginless'>
+                            <div className='field'>
+                                <label id='label-email'>Email</label>
+                                <input id='input-email' onChange={e => onChange(e)} value={email} type='email' name='email'/>
+                            </div>
+                        </div>
+                        <div className='column is-marginless'>
+                            <div className='field'>
+                                <label id='label-password'>Password</label>
+                                <input id='input-password' onChange={e => onChange(e)} value={password} type='password' name='password'/>
+                            </div>
+                        </div>
 
-                <div className='columns is-justify-center'>
+                        <div id='container-login-register' className='columns is-direction-column'>
+                            <div className='column is-marginless'>
+                                <button id='btn-login' type='submit' className='button is-block is-fullwidth is-rounded'>Login</button>
+                            </div>
+                            <div className='column is-marginless'>
+                                <button id='btn-register' type='submit' className='button is-block is-fullwidth is-rounded'>Register</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
+                </div>
+            </div>
+
+            {/*<div className='container'>
+              <div className='columns is-justify-center'>
                     <div className='column is-half'>
                         <div className='cards'>
                             <h2 id='title-login'> Login </h2>
@@ -60,7 +93,7 @@ const Login = ({ login, isAuthenticated }) => {
                     </div>
                 </div>
 
-            </div>
+             </div> */}
         </Fragment>
     )
 
